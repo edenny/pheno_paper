399b5a1ae519b57595292899d374e3582da9d934
John Deck <jdeck88@gmail.com>
2017-04-13 13:29:10 -0700

    adding splitter code. this only works for NPN so need to copy this functionality for PEP just need to update the PEP script with the proper call to bin/Utils.py-splitter function

--- bin/runFiles.sh
+++ bin/runFiles.sh
@@ -26,27 +26,29 @@ function runLoop {
     for file in "${files[@]}"
     do
         # triplify
-        make -f ../Makefile ppo-fims-triples file_name=../data/$project/output_csv/$file output_directory=../data/$project/output_unreasoned_n3/ configuration_file=../$project/$project.xml
+        make -f ../Makefile ppo-fims-triples file_name=../data/$project/output_csv_split/$file output_directory=../data/$project/output_unreasoned_n3/ configuration_file=../$project/$project.xml
         # reason
         make -f ../Makefile reasoner project_name=$project file_name=$file.n3 ontopilot=../ontopilot/bin/ontopilot.py ppo_pre_reasoner_dir=../../ppo_pre_reasoner/ base_input_dir=../pheno_paper/data/$project/output_unreasoned_n3/ output_file=../pheno_paper/data/$project/output_reasoned_owl/$file.owl
     done
 }
 
-#runLoop npn $files
-
+####################
 # Helianthus files
-#files=( 1485013283920.csv )
-#runLoop npn $files
-
+####################
+files=( npntest.csv )
+runLoop tests $files
 #files=( Helianthus.csv )
 #runLoop pep725 $files
 
+####################
 # Betula files
-files=( 1485012823554.csv )
-runLoop npn $files
+####################
+#files=( Betula_1.csv Betula_2.csv Betula_3.csv Betula_4.csv Betula_5.csv Betula_6.csv Betula_7.csv )
+#runLoop pep725 $files
+#files=( 1485012823554_1.csv 1485012823554_2.csv 1485012823554_3.csv 1485012823554_4.csv )
+#runLoop npn $files
 
-files=(  Betula.csv )
-runLoop pep725 $files
+# Not sure if these are needed:
 #files=(  Betula_1.csv Betula_2.csv Betula_3.csv Betula_4.csv)
 #runLoop pep725 $files
 #files=( 1485012823554_1.csv 1485012823554_2.csv)
