08c6ac5bc02494cb1a0b1a52730dd91d72145054
John Deck <jdeck88@gmail.com>
2017-04-17 12:53:41 -0700

    making a tests directory off of the root directory.  Also made a test python script for comparing test output.

--- bin/runFiles.sh
+++ bin/runFiles.sh
@@ -17,6 +17,7 @@
 #       >pep725
 #    >bin
 
+ontopilot=./ontopilot/bin/ontopilot.py
 
 # execute the triplify and reason functions for a given project and list of files
 function runLoop {
@@ -26,19 +27,22 @@ function runLoop {
     for file in "${files[@]}"
     do
         # triplify
-        make -f ../Makefile ppo-fims-triples file_name=../data/$project/output_csv_split/$file output_directory=../data/$project/output_unreasoned_n3/ configuration_file=../$project/$project.xml
+        make -f ../Makefile ppo-fims-triples file_name=../data/$project/output_csv_split/$file output_directory=../data/$project/output_unreasoned_n3/ configuration_file=../$project/$project.xml format=TURTLE
         # reason
-        make -f ../Makefile reasoner project_name=$project file_name=$file.n3 ontopilot=../ontopilot/bin/ontopilot.py ppo_pre_reasoner_dir=../../ppo_pre_reasoner/ base_input_dir=../pheno_paper/data/$project/output_unreasoned_n3/ output_file=../pheno_paper/data/$project/output_reasoned_owl/$file.owl
+        make -f ../Makefile reasoner project_name=$project file_name=$file.n3 ontopilot=$ontopilot ppo_pre_reasoner_dir=../../ppo_pre_reasoner/ base_input_dir=../pheno_paper/data/$project/output_unreasoned_n3/ output_file=../pheno_paper/data/$project/output_reasoned_owl/$file.owl
     done
 }
 
+function runTriplifyTest {
+	# run Triplifier
+        make -f ../Makefile ppo-fims-triples file_name=../tests/triplifier_input/npntest.csv output_directory=../tests/triplifier_actual_output/ configuration_file=../tests/tests.xml format=N-TRIPLE
+	# compare outpt
+}
+
 ####################
-# Helianthus files
+# Tests
 ####################
-files=( npntest.csv )
-runLoop tests $files
-#files=( Helianthus.csv )
-#runLoop pep725 $files
+runTriplifyTest
 
 ####################
 # Betula files
@@ -48,8 +52,3 @@ runLoop tests $files
 #files=( 1485012823554_1.csv 1485012823554_2.csv 1485012823554_3.csv 1485012823554_4.csv )
 #runLoop npn $files
 
-# Not sure if these are needed:
-#files=(  Betula_1.csv Betula_2.csv Betula_3.csv Betula_4.csv)
-#runLoop pep725 $files
-#files=( 1485012823554_1.csv 1485012823554_2.csv)
-#runLoop npn $files
---
+++ tests/runTriplifierTest.py
@@ -0,0 +1,75 @@
+#!/bin/python
+# A simple set-comparison script to test input and output from triplification process
+
+import argparse
+import os
+import tempfile
+
+# a function to loop and print a set
+def loopSet(mySet):
+    # convert set to list
+    myList = list(mySet)
+    for item in myList:
+        print "    "+item
+
+
+# Argument parser
+parser = argparse.ArgumentParser(description='Test unreasoned triple output')
+#parser.add_argument('actual_filename', type=argparse.FileType('r'), help='the file containing the actual results')
+#parser.add_argument('expected_filename', type=argparse.FileType('r'), help='thie file containing expected results')
+parser.add_argument('actual_filename', help='the file containing the actual results')
+parser.add_argument('expected_filename', help='thie file containing expected results')
+
+# argparser parser automatically checks for correct input from the command line
+args = parser.parse_args()
+
+print args.actual_filename
+# convert turtle format to ntriples format
+print "converting turtle to ntriples ..."
+tf = tempfile.NamedTemporaryFile(mode='w')
+#print(os.path.realpath(args.actual_filename.name))
+print (tf.name)
+os.system(os.path.realpath(tf.name))
+os.system("rapper -i turtle -o ntriples " + os.path.realpath(args.actual_filename) + " > " + os.path.realpath(tf))
+
+# compare actual and expected filenames
+actual=set(line.strip() for line in open(tf.readlines()))
+expected=set(line.strip() for line in open(expected_filename.readlines()))
+
+# compute set differences
+notInActual = actual-expected
+notInExpected = expected-actual
+
+# print output
+print "********************************************************************"
+print "* Test frame-work for test data that has been triplified"
+print "* These tests operate on files that have been pre-processed and "
+print "* then converted into triples, effectively testing the output of the"
+print "* triplifier."
+print "********************************************************************"
+
+print ""
+allPassed=True
+
+if (bool(notInActual)):
+    print "* Lines appearing in '" +args.actual_filename.name +"' but not in '"+args.expected_filename.name +"'"
+    print "* These are unexpected triples being written."
+    loopSet(notInActual)
+    allPassed=False
+    print ""
+
+
+if (bool(notInExpected)):
+    print "* Lines appearing in '" +args.expected_filename.name +"' but not in '"+args.actual_filename.name +"'"
+    print "* These are triples that should have been written, but were not."
+    allPassed=False
+    loopSet(notInExpected)
+
+if (allPassed):
+    print ""
+    print "All tests passed!"
+    print ""
+else:
+    print ""
+    print "Some tests failed"
+    print ""
