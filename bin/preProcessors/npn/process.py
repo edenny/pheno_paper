7b230afc7139b00f47e502029fa47ca2085e99b6
John Deck <jdeck88@gmail.com>
2017-05-02 09:46:35 -0700

    putting intensity_value ranges in its own lookup file, makes code easier to re-use and for others to see values

--- bin/preProcessors/npn/process.py
+++ bin/preProcessors/npn/process.py
@@ -2,6 +2,8 @@
 # this script reads NPN files that have been downloaded and stored in input
 # directories and performs necessary pre-processing steps before triplification
 # can occur.
+import csv
+import inspect
 import argparse
 import pandas as pd
 import numpy as np
@@ -18,27 +20,29 @@ class processNPN:
         self.outputDir = outputDir 
         self.mainIndexName = 'record_id'
 
+        self.cur_dir = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))+'/'
         # initialize directory path
         if (os.path.exists(self.outputDir)):
             shutil.rmtree(self.outputDir)
         os.makedirs(self.outputDir)
 
-    # apply an lower count value where there is no intensity value and phenophase_status is 1
-    def status1NoIntensity(self,row):
-        if (row['Phenophase_Status'] == 1 and row['Intensity_Value'] == '-9999'):
-            return 1
-        else:
-            # return default value
-            return row['lower_count']
-    
-    # apply an upper count value where there is no intensity value and phenophase_status is 0
-    def status0NoIntensity(self,row):
-        if (row['Phenophase_Status'] == 0 and row['Intensity_Value'] == '-9999'):
-            return 0
-        else:
-            # return default value
-            return row['upper_count']
-    
+    # Function to read in a CSV file containing one or more values that we want
+    # to use to translate values  for.  using the dataframe's "lookupColumn"
+    # as the key
+    def translate(self,filename,cols,index_name,dataFrame,lookupColumn):
+        # loop all columns
+        for column in cols:
+            # don't look at index column
+            if (column != index_name):
+                # read the incoming lookup filename into a dictionary using the 
+                # the appropriate columns to assign the dictionary key/value
+                with open(filename) as f:
+                    thisDict = dict((rows[cols.index(index_name)],rows[cols.index(column)]) \
+                            for rows in csv.reader(f))
+                    # assign the new column name values based on lookup column name
+                    dataFrame[column] = dataFrame[lookupColumn].map(thisDict)
+        return dataFrame
+
     def process(self):
         framesDict = {}
         # Loop each directory off of input directory
@@ -60,30 +64,16 @@ class processNPN:
 
                         # Add an index name 
                         df.index.name = self.mainIndexName
-                        # This section maps all Intensity_Value values to lower/upper counts and lower/upper percentages
-                        df['lower_count'] = ''
-                        df['upper_count'] = ''
-                        df['lower_percent'] = ''
-                        df['upper_percent'] = '' 
-                        # map lower counts
-                        equiv = {'1,001 to 10,000':1001, '101 to 1,000':101, '11 to 100':11, '3 to 10':3, 'Less than 3':0, 'Little':1, 'Lots':1, 'More than 10':11, 'More than 10,000':10001, 'Some':1, '25-49%':'', '5-24%':'', '50-74%':'', '75-94%':'', '95% or more':'', 'Less than 25%':'', 'Less than 5%':'','-9999':''}
-                        df['lower_count'] = df['Intensity_Value'].map(equiv)
-                        # map upper counts
-                        equiv = {'1,001 to 10,000':10000, '101 to 1,000':1000, '11 to 100':100, '3 to 10':10, 'Less than 3':2, 'Little':'', 'Lots':'', 'More than 10':'', 'More than 10,000':'', 'Some':'', '25-49%':'', '5-24%':'', '50-74%':'', '75-94%':'', '95% or more':'', 'Less than 25%':'', 'Less than 5%':'','-9999':''}
-                        df['upper_count'] = df['Intensity_Value'].map(equiv)
-                        # map lower percents
-                        equiv = {'1,001 to 10,000':'', '101 to 1,000':'', '11 to 100':'', '3 to 10':'', 'Less than 3':'', 'Little':'', 'Lots':'', 'More than 10':'', 'More than 10,000':'', 'Some':'', '25-49%':.25, '5-24%':.05, '50-74%':.50, '75-94%':.75, '95% or more':.95, 'Less than 25%':.0, 'Less than 5%':.0,'-9999':''}
-                        df['lower_percent'] = df['Intensity_Value'].map(equiv)
-                        # map upper percents
-                        equiv = {'1,001 to 10,000':'', '101 to 1,000':'', '11 to 100':'', '3 to 10':'', 'Less than 3':'', 'Little':'', 'Lots':'', 'More than 10':'', 'More than 10,000':'', 'Some':'', '25-49%':.49, '5-24%':.24, '50-74%':.74, '75-94%':.94, '95% or more':1.00, 'Less than 25%':.249, 'Less than 5%':.049,'-9999':''}
-                        df['upper_percent'] = df['Intensity_Value'].map(equiv)
+
+                        # Translate values from intensity_values.csv file 
+                        cols = ['value','lower_count','upper_count','lower_percent','upper_percent']
+                        # translate values
+                        df = self.translate(self.cur_dir +'/intensity_values.csv',cols,'value',df,'Intensity_Value')
     
                         # In cases where the Intensity_Value = -9999 and Phenophase_Status = 0 the 'upper count' should be 0
-                        #df['upper_count'] = df.apply(self.status0NoIntensity,axis=1)
 
                         df.loc[(df.Intensity_Value == '-9999') & (df.Phenophase_Status == '0'),'upper_count'] = 0
                         # In cases where the Intensity_Value = -9999 and Phenophase_Status = 1 the 'lower count' should be 1
-                        #df['lower_count'] = df.apply(self.status1NoIntensity,axis=1)
                         df.loc[(df.Intensity_Value == '-9999') & (df.Phenophase_Status == '1'),'lower_count'] = 1
                         df['Source'] = 'NPN'
     
